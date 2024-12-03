ea951ca051eb46dee940c712197a38f511af0894
fprtkdl <beatpunchbeef141211@gmail.com>
2024-12-03 10:51:12 +0900

    router
    설정한 것 같다

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import UserView from '../views/User.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,10 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: AboutView
     },
+    {
+      path: '/user/:userName',
+      component: UserView
+    }
   ],
 })
 
